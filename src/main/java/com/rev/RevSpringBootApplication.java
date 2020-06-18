5ccc2484a3c7470350aae2f7973f1bfc89b5764c
Samuel Lima <samuellima810@gmail.com>
2020-06-18 01:51:55 -0300

    mais um teste

--- src/main/java/com/rev/RevSpringBootApplication.java
+++ src/main/java/com/rev/RevSpringBootApplication.java
@@ -3,9 +3,13 @@ package com.rev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.rev.config.ConfigProducaoHttps;
+
 @SpringBootApplication
+@EnableConfigurationProperties(ConfigProducaoHttps.class)
 public class RevSpringBootApplication {
 
 	public static void main(String[] args) {		
--- src/main/java/com/rev/config/JpaConfig.java
+++ src/main/java/com/rev/config/JpaConfig.java
@@ -15,43 +15,35 @@ import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 
-@Configuration
+//@Configuration
 public class JpaConfig {
-
-	@Profile("local")
-	@Bean
-	public DataSource dataSource() {
-		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
-		dataSourceLookup.setResourceRef(true);
-		return dataSourceLookup.getDataSource("jdbc/projetobase");
-	}
-	
-	@Profile("prod")
-	@Bean
-	public DataSource dataSourceProd() throws URISyntaxException {
-		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
-
-	    String username = jdbUri.getUserInfo().split(":")[0];
-	    String password = jdbUri.getUserInfo().split(":")[1];
-	    String port = String.valueOf(jdbUri.getPort());
-	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
-	    
-	    BasicDataSource dataSource = new BasicDataSource();
-	    dataSource.setUrl(jdbUrl);
-	    dataSource.setUsername(username);
-	    dataSource.setPassword(password);
-	    dataSource.setInitialSize(10);
-	    return dataSource;
-	}
-	
-	@Bean
-	public JpaVendorAdapter jpaVendorAdapter() {
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(false);
-		adapter.setGenerateDdl(false);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-		return adapter;
-	}
-	
+	/*
+	 * @Profile("local")
+	 * 
+	 * @Bean public DataSource dataSource() { JndiDataSourceLookup dataSourceLookup
+	 * = new JndiDataSourceLookup(); dataSourceLookup.setResourceRef(true); return
+	 * dataSourceLookup.getDataSource("jdbc/projetobase"); }
+	 * 
+	 * @Profile("prod")
+	 * 
+	 * @Bean public DataSource dataSourceProd() throws URISyntaxException { URI
+	 * jdbUri = new URI(System.getenv("JAWSDB_URL"));
+	 * 
+	 * String username = jdbUri.getUserInfo().split(":")[0]; String password =
+	 * jdbUri.getUserInfo().split(":")[1]; String port =
+	 * String.valueOf(jdbUri.getPort()); String jdbUrl = "jdbc:mysql://" +
+	 * jdbUri.getHost() + ":" + port + jdbUri.getPath();
+	 * 
+	 * BasicDataSource dataSource = new BasicDataSource();
+	 * dataSource.setUrl(jdbUrl); dataSource.setUsername(username);
+	 * dataSource.setPassword(password); dataSource.setInitialSize(10); return
+	 * dataSource; }
+	 * 
+	 * @Bean public JpaVendorAdapter jpaVendorAdapter() { HibernateJpaVendorAdapter
+	 * adapter = new HibernateJpaVendorAdapter();
+	 * adapter.setDatabase(Database.MYSQL); adapter.setShowSql(false);
+	 * adapter.setGenerateDdl(false);
+	 * adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect"); return
+	 * adapter; }
+	 */	
 }
