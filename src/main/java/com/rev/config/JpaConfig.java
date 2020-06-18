846198b8f43a63b8b64716d00f189296d7a136a7
Samuel Lima <samuellima810@gmail.com>
2020-06-18 00:45:35 -0300

    adicionando uma class jpa e dependencia no pom

---
+++ src/main/java/com/rev/config/JpaConfig.java
@@ -0,0 +1,57 @@
+package com.rev.config;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.lookup.JndiDataSourceLookup;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+
+@Configuration
+public class JpaConfig {
+
+	@Profile("local")
+	@Bean
+	public DataSource dataSource() {
+		JndiDataSourceLookup dataSourceLookup = new JndiDataSourceLookup();
+		dataSourceLookup.setResourceRef(true);
+		return dataSourceLookup.getDataSource("jdbc/projetobase");
+	}
+	
+	@Profile("prod")
+	@Bean
+	public DataSource dataSourceProd() throws URISyntaxException {
+		URI jdbUri = new URI(System.getenv("JAWSDB_URL"));
+
+	    String username = jdbUri.getUserInfo().split(":")[0];
+	    String password = jdbUri.getUserInfo().split(":")[1];
+	    String port = String.valueOf(jdbUri.getPort());
+	    String jdbUrl = "jdbc:mysql://" + jdbUri.getHost() + ":" + port + jdbUri.getPath();
+	    
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setUrl(jdbUrl);
+	    dataSource.setUsername(username);
+	    dataSource.setPassword(password);
+	    dataSource.setInitialSize(10);
+	    return dataSource;
+	}
+	
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(false);
+		adapter.setGenerateDdl(false);
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		return adapter;
+	}
+	
+}
